0d10a256a5e6ff5a32ad341223d2752514f4a998
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-08-30 17:43:03 -0300

    Configurando arquivos admin/models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'created_date',)
+    ordering = '-id'
+    list_filter = 'created_date'
+    search_fields = ('id', 'first_name', 'last_name')
+    list_per_page = 10
+    list_max_show_all = 100
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone  # type: ignore
 
 # Create your models here.
+
+
+class Contact(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
