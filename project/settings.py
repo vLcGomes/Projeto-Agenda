80d986ebe879ae8823d149f3af66d4502e891551
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-09-02 13:09:01 -0300

    Atualizações nos arquivos de css, adição de HTML no arquivo index.html

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kp=i%gofqt!*q=@df&v#avqwoxj%um(7f+%#55p=$x%gu2rz(n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
