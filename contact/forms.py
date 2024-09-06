fa783ee6e32b0453bcd87007ae2862988f10ace5
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-09-06 12:13:05 -0300

    Atualizações no formulário de cadatro, inserindo os campos que faltavam

--- contact/forms.py
+++ contact/forms.py
@@ -4,17 +4,50 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui',
+            }
+        ),
+        help_text='Texto de ajuda para seu usuario'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = ('first_name', 'last_name', 'phone',
+                  'email', 'description', 'category')
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error('first_name', ValidationError(
-            'Mendagem de erro', code='invalid'))
-
-        self.add_error('first_name', ValidationError(
-            'Mendagem de erro 2', code='invalid'))
+        if first_name == last_name:
+            msg = ValidationError(
+                'Nome e Sobrenome não podem ser iguais',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+
+    def clean_first_name(self):
+        cleaned_data = self.cleaned_data.get('first_name')
+
+        if cleaned_data == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Nome Inválido',
+                    code='invalid'
+                )
+            )
+
+        return cleaned_data
