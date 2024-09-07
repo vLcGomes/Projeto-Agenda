d6aee638f3a4df8e20fa9a081d42c6d7d280d8bd
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-09-07 12:32:58 -0300

    Finalizando configurações de Login

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@ from django.contrib import messages  # type: ignore
 from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth  # type: ignore
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -25,6 +26,7 @@ def register(request):
     )
 
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
 
@@ -68,6 +70,7 @@ def login_view(request):
     )
 
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
 
